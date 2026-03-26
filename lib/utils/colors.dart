f9c2c8d92090ad21474698ccf6cb4f52a571d024
Daud Bin Nasar <64163448+rajadaud12@users.noreply.github.com>
2026-03-26 07:11:04 +0500

    Update lib/utils/colors.dart via MYTH Build System

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF2196F3);
+const primaryLight = Color(0xFFBBDEFB);
+const secondaryColor = Color(0xFF4CAF50);
+const errorColor = Color(0xFFF44336);
+const backgroundColor = Color(0xFFFFFFFF);
+const textPrimary = Color(0xFF121212);
+const textSecondary = Color(0xFF666666);
